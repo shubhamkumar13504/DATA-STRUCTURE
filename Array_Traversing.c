37c0109b0090bc7532b69c192e085d8769382c04
SHUBHAM KUMAR <shubhamkumar13504@gmail.com>
2025-06-17 00:40:43 +0530

    Update Array_Traversing.c

--- Array_Traversing.c
+++ Array_Traversing.c
@@ -8,16 +8,9 @@ int main()
     {
         printf("enter values at position %d :  ", i + 1);
         scanf("%d", (arr + i)); // ( arr+i / &arr[i] )
-    }
-
-
-
-
-
-    
+    } 
     // Traversing
     for (int i = 0; i < size; i++)
-
     {
         printf("values are: %d \n", arr[i]);
     }
